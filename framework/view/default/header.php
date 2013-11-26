a9500662423fdd3a0dfec5d4f14d8c969a3ee961
NuNing <suriyaporna@gmail.com>
2013-11-25 23:36:59 -0500

    header_updated

--- framework/view/default/header.php
+++ framework/view/default/header.php
@@ -3,13 +3,12 @@
 ?>
 
 <div id="header">																																																																								
-	<img <?php echo('src="' . "http://localhost/rnj/framework/file/images/ban.png" . '"'); ?> alt="banner" width="1000" height="222" border="0" usemap="#Map" />
+	<img <?php echo('src="' . "http://localhost/rnj/framework/file/images/ban.png" . '"'); ?> alt="banner" width=1000 height="222" border="0" usemap="#Map" />
 	<BR>																																										
-	
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css"> 
 	<ul id="menu">
 		<li><a href="index.php" class="but1_active">Home</a></li>
-		<li><a href="registration.php" class="but2_active">Register</a></li>
-		<li><a href="employee_login.php" class="but3_active">Employee</a></li>
-		<li><a href="search.php" class="but5_active">Search</a></li>
+		<li><a href="signup.php" class="but2_active">Register</a></li>
+		<li><a href="login.php" class="but3_active">Log in</a></li>
 	</ul>
 </div>
