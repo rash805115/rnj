af956e5e66014e3ae92185df7ef18c4aebd16d1b
NuNing <suriyaporna@gmail.com>
2013-11-27 14:41:19 -0500

    user/login_updated

--- framework/view/default/user/login.php
+++ framework/view/default/user/login.php
@@ -9,33 +9,41 @@
 	</head>
 
 	<body>
+            <div id="wrapper">
 		<?php include (__DIR__ . "/../../default/include.php"); ?>
-		
-		<div name="login-div" id="login-div">
-			<form method="POST" action="" name="login-form" id="login-form" onsubmit="return check('login-form', 'checkForBlanks');">
-				<table name="login-table" id="login-table">
-					<tr name="user-field" id="user-field">
-						<td><label>Username:</label></td>
-						<td><input type="text" name="user" id="user" maxlength="32" onblur="return verifyUserID('user');"></td>
-					</tr>
-					<tr name="pass-field" id="pass-field">
-						<td><label>Password:</label></td>
-						<td><input type="password" name="pass" id="pass" maxlength="32"></td>
-					</tr>
-					<tr name="remember-me-field" id="remember-me-field">
-						<td><label>Remember Me:</label></td>
-						<td><input type="checkbox" name="remember-me" id="remember-me"></td>
-					</tr>
-					<tr name="checkout-field" id="checkout-field">
-						<td><input type="submit" name="submit" id="submit" value="Submit"></td>
-						<td><input type="reset" name="reset" id="reset" value="Reset"></td>
-					</tr>
-				</table>
-			</form>
-		</div>
-		
-		<BR><a <?php $forgotpasswordURL = \phpsec\HttpRequest::Protocol() . "://" . \phpsec\HttpRequest::Host() . \phpsec\HttpRequest::PortReadable() . "/rnj/framework/forgotpassword"; echo "href='{$forgotpasswordURL}'"; ?> >Forgot Password</a> Click Here to recover your access to account in case you have forgot your password.
-		
-		<script type="text/javascript" <?php echo('src="' . "http://localhost/rnj/framework/file/js/check.js" . '"'); ?> ></script>
-	</body>
+																																																																
+                <div id="content_inside">
+                    <div id="main_block">
+                        <div class="news">
+                            <div name="login-div" id="login-div">
+                                <h1>Customer Login</h1><br/>
+                                    <form method="POST" action="" name="login-form" id="login-form" onsubmit="return check('login-form', 'checkForBlanks');">
+                                            <table name="login-table" id="login-table">
+                                                    <tr name="user-field" id="user-field">
+                                                            <td><label>Username:</label></td>
+                                                            <td><input type="text" name="user" id="user" maxlength="32" onblur="return verifyUserID('user');"></td>
+                                                    </tr>
+                                                    <tr name="pass-field" id="pass-field">
+                                                            <td><label>Password:</label></td>
+                                                            <td><input type="password" name="pass" id="pass" maxlength="32"></td>
+                                                    </tr>
+                                                    <tr name="remember-me-field" id="remember-me-field">
+                                                            <td><label>Remember Me:</label></td>
+                                                            <td><input type="checkbox" name="remember-me" id="remember-me"></td>
+                                                    </tr>
+                                                    <tr name="checkout-field" id="checkout-field">
+                                                            <td><input type="submit" name="submit" id="submit" value="Submit"></td>
+                                                            <td><input type="reset" name="reset" id="reset" value="Reset"></td>
+                                                    </tr>
+                                            </table>
+                                    </form>
+                            </div>
+                            <BR><a <?php $forgotpasswordURL = \phpsec\HttpRequest::Protocol() . "://" . \phpsec\HttpRequest::Host() . \phpsec\HttpRequest::PortReadable() . "/rnj/framework/forgotpassword"; echo "href='{$forgotpasswordURL}'"; ?> >Forgot Password</a> <br> Click Here to recover your access to account in case you have forgot your password.
+                            <script type="text/javascript" <?php echo('src="' . "http://localhost/rnj/framework/file/js/check.js" . '"'); ?> ></script>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                        
+     </body>
 </html>
