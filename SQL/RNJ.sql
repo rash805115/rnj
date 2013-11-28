82948d73db232deb17fb1daaf1d3bcb69fe22489
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2013-11-28 03:21:14 -0500

    updated SQL

--- SQL/RNJ.sql
+++ SQL/RNJ.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 27, 2013 at 09:37 PM
+-- Generation Time: Nov 28, 2013 at 03:20 AM
 -- Server version: 5.5.22
 -- PHP Version: 5.3.10-1ubuntu3
 
@@ -176,7 +176,8 @@ INSERT INTO `PASSWORD` (`TEMP_PASS`, `USE_FLAG`, `TEMP_TIME`, `TOTAL_LOGIN_ATTEM
 ('8bf6a90306', 1, 0, 1, 1382234585, 1382234585, 'rash'),
 ('8f47155b0d97f3d01a00cec52fd461e50a7af8a2735c87fce6fe1eabb0bb49fd46ae2d8f8912279deb3cd4b8992e72bf46dd4f1509f9edd737de3be140db3baf', 0, 1382233239, 0, 0, 0, 'root'),
 ('2bff93df10', 1, 0, 0, 0, 0, 'jack'),
-('59e8710928', 1, 0, 0, 0, 0, 'ning');
+('59e8710928', 1, 0, 0, 0, 0, 'ning'),
+('11e7690bcc', 1, 0, 0, 0, 0, 'ning123');
 
 -- --------------------------------------------------------
 
@@ -216,7 +217,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `imageurl` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`pid`),
   KEY `kkid` (`kkid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `product`
@@ -228,7 +229,9 @@ INSERT INTO `product` (`pid`, `kkid`, `pname`, `tinventory`, `price`, `imageurl`
 (10, 1, 'Samsung Monitor', 1, 275, '10.jpg'),
 (11, 3, 'Harry Potter and the Sorcerer''s Stone', 2, 14.99, '1.jpg'),
 (12, 3, 'Harry Potter and the Chamber of Secrets', 5, 13.99, '2.gif'),
-(13, 3, 'Harry Potter and the Prisoner of Azkaban', 3, 15.99, '3.jpg');
+(13, 3, 'Harry Potter and the Prisoner of Azkaban', 3, 15.99, '3.jpg'),
+(14, 2, 'HP Laptop', 1, 399, '3.jpg'),
+(15, 2, 'Dell Laptop', 5, 699, '4.jpg');
 
 -- --------------------------------------------------------
 
@@ -316,12 +319,12 @@ INSERT INTO `SESSION` (`SESSION_ID`, `DATE_CREATED`, `LAST_ACTIVITY`, `USERID`) 
 ('6532106cec75b95900be5dc5346bad1c7579764029438bbeebd650abd37e5cbd18c4b45131bff53ad98ccac8e8591327a1e2cab5ad0df78c26fa3cb36b60bfc7', 1381999678, 1381999678, 'rash'),
 ('6b9f99b79142a60c5e2fdc322e2c517a2b35daf7ba0655f2ed01c70bdd3b0a2ac3a0fcf07c9cfa214e98bed3a6785985a9d3e23a552f835d77329f29e4a9edcd', 1382811781, 1382811781, 'rash'),
 ('8191dc91c2d9b14307117a4c13362cc698f43d15bc4026d4bc40cf4ae127e0e30d36b0b17a7dd76e086ff04547112fcb77d05930ae9d96cb7afd50e4eb6cca53', 1382044893, 1382044893, 'rash'),
+('8c3d98177a2d83adfaa75f4da05f6fff3d37dee13410de2219801e7099101bb83a916b9c1a6dbb0c7a96ceda6f4118d137350ac9d03b6c29518bf432c483116a', 1385626624, 1385626751, 'rash'),
 ('9d11bfc31367eafeb7c02187f4713436f6d9b3b6a0a57eed6d8b744829b6c492315a577c09a2febc6e50b4ad85a321200bd0e44267cd55570a70d6d219b57be4', 1382097240, 1382097240, 'rash'),
 ('aeb33f4ba8a072004db2d6317e9597520fb422a0c74c208947eedd29465ef2183c17c1978f2ddc81eac23b24239de00baa0e47b1fe35665946bfecf225ea0f75', 1381999575, 1381999575, 'rash'),
 ('b21d332d9eff86aae39437fb3ad01c846801b8f9d352b8ec06d00ef404cf5764e4bcbc1675d4b0809335a54c8255289bcc5b97bc164ea5df3f7d3c3dfdba5795', 1381999727, 1381999727, 'rash'),
 ('b622693dcb4a2bf900068751510f4d29683dd3d63041460c0659a0e94b87d150645d7e8811f280b4e1bde395e42fda59a3613ef7aeecd4bcc1355ee93cd0ad12', 1382041251, 1382041251, 'rash'),
-('f770b481aeb079ec5a089fd4dc169a01df059f39774f3fa0ebd3969681f89c6c63f174a9724936d4fa5e016cf82589fdccec10a2f01de9fed379a2b9a8664ede', 1382046316, 1382046316, 'rash'),
-('fc2336702b194d863604f5a5107f6f4dbbfcab2cb47b8f6aba69cfbe823097860ba9b07b36fb16c6a374edcd4f70c4425790c1751b8ee952bdc30403210ddff7', 1385410189, 1385410189, 'rash');
+('f770b481aeb079ec5a089fd4dc169a01df059f39774f3fa0ebd3969681f89c6c63f174a9724936d4fa5e016cf82589fdccec10a2f01de9fed379a2b9a8664ede', 1382046316, 1382046316, 'rash');
 
 -- --------------------------------------------------------
 
@@ -409,6 +412,7 @@ CREATE TABLE IF NOT EXISTS `USER` (
 INSERT INTO `USER` (`USERID`, `P_EMAIL`, `ACCOUNT_CREATED`, `LOCKED`, `INACTIVE`, `HASH`, `DATE_CREATED`, `ALGO`, `DYNAMIC_SALT`) VALUES
 ('jack', 'rahul.chaudhary@owasp.org', 1382636773, 1, 0, '55dbd88705076de6aac4b27b53cb7495a3927f7eb076f34d7f0b10787767d12a11e8e1b36e39f5dcf93447749afd252eb4735d5900a9a6e77d859dcb7db91bf5', 1382636773, 'sha512', '9c44ce028fb26cf72681a3a0940dbbb508b8207c95121b1366e542fc2c66b3aac5dd7e8e6867b2d64ebf04c71fa123abc646cfb3f6cfc1bb4d9e2e0eade61715'),
 ('ning', 'rahul300chaudhary400@gmail.com', 1385409875, 0, 0, 'b01b0e9af71634a23aa0cfe5df52dac2311ffd278714aa4233e07243085894cefc779756f9bfb966bd5cc747639d1d8cb82d7ab31f95b3627bc7bfd7dfbb0802', 1385409875, 'sha512', '6bc3c3af402313a8899cf8367bb5c86a94c70c2b570f348987ec1da72f195d9d8e9c4a225128593c23e2c22014ff402afd6519050bbb8a7f6bb82732a401a2ad'),
+('ning123', 'rahul300chaudhary400@gmail.com', 1385622426, 0, 0, '5c49393e3fd7c2fe67dbc7d07e9ea46ae0231d3c9b0e3288e3d24355e3f02f12291d5fd3ef509841ee4e92c1878ee29633de22a09cca55b9ec270b48d56a1f99', 1385622426, 'sha512', 'cac9896e9284c39c0d34f2bddc29734bed0b86c40ff7aeea516534a12a2a0affd34c43125d518af1a0c4bd821779d3a46f172cc2b5a4fef46eda6537023496cb'),
 ('owasp', 'rash@a.com', 1384763795, 0, 1, 'b20c4f48237727ffaf05ae94ce46a3e24dd3027fbb099706d3f9657d0bc6c8aca1bf1aadbc54670359affb920433ec28457727ef3b76d47e1e8740671c9522a7', 1384763795, 'sha512', '34c79dc83ca4a897e3a353988379b41168d21a4ddb172eda0ee017f9d3d57f14abcf3ad848544b5b3a0d5880c1575525e4596ab25159713259223d8a35b253f1'),
 ('rash', 'rahul.chaudhary@owasp.org', 1381998328, 0, 0, 'b6b253f620bf3afcd56933058e2bdb3b6c015ef474a2388fe0b9c969fe5fc70906441d3c499eb06520c513807c2afd4b7c4b47d0175319fc06e5c8300b750f63', 1382233652, 'sha512', '8b140860ae9a43caa783ce1a9aabdefc21613e8dd09b1425d0dc2c1f3ec2a2559ee5419a8e4496f5bce4583415ac3ddc8cd8727e9b8e3039352d8a1e6474c024'),
 ('root', 'rac130@pitt.edu', 1382232424, 0, 1, 'c99d0727ab89828f79d81689c7a53f95a34d92bccd133ae7a38354e8bc5b737c843cecccfa949b2fcce59c141f953f1d849dd5ef7c9b965594f84cb6313bb99a', 1382232424, 'sha512', '3963caa6707112b947e1425dd408b22e47f2b28f41ac9a5fdea301a47789ecfe6df2f9988a19edd9427227fe6addc79f23af061e7b6c241c79a82e2ffa623b8d');
@@ -451,6 +455,14 @@ CREATE TABLE IF NOT EXISTS `user_interested_product` (
   PRIMARY KEY (`USERID`,`pid`),
   KEY `pid` (`pid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_interested_product`
+--
+
+INSERT INTO `user_interested_product` (`USERID`, `pid`) VALUES
+('rash', 8),
+('rash', 11);
 
 -- --------------------------------------------------------
 
